0afdaa76695c206cf2c61500fa494ba8dea64832
lilyanovi <zauzolkova.lilia@ya.ru>
2023-01-24 14:17:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log('Record 1'); 
+
+setTimeout(() => { 
+    console.log('Record 2'); 
+    Promise.resolve()
+        .then(() => { 
+            setTimeout(() => { 
+                сonsole.log('Record 3'); 
+                Promise.resolve()
+                    .then(() => { 
+                        console.log('Record 4');
+                    }); 
+            }); 
+        }); 
+}); 
+
+console.log('Record 5'); 
+
+Promise.resolve().then(() => Promise.resolve()
+    .then(()=>console.log('Record 6')));
